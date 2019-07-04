ce49f38d5808c386b7bbc63a53b2878365d1ddf8
天翔 渡邉 <kake0918@outlook.jp>
2019-07-04 17:49:54 +0900

    #add VirtualServerクラスを追加

---
+++ MainProject/KosenProcon30/VirtualServer.cpp
@@ -0,0 +1,1 @@
+#include "VirtualServer.hpp"
---
+++ MainProject/KosenProcon30/VirtualServer.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "KosenProcon30.hpp"
+#include "Field.hpp"
+#include "Team.hpp"
+#include "Agent.hpp"
+namespace Procon30 {
+	class VirtualServer
+	{
+
+	};
+}
