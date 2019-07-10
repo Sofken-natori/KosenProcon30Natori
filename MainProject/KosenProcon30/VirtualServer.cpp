a09e4f1aa6c268d22a8065307cf152eb2269f4fd
kakeru <kake0918@outlook.jp>
2019-07-10 18:29:32 +0900

    #change random_number(),write_json(),input_point()の追加など

--- MainProject/KosenProcon30/VirtualServer.cpp
+++ MainProject/KosenProcon30/VirtualServer.cpp
@@ -1,1 +1,24 @@
 #include "VirtualServer.hpp"
+
+int32 Procon30::VirtualServer::random_number()
+{
+		return Random(1, 6);
+}
+
+void Procon30::VirtualServer::write_json()
+{
+
+}
+
+void Procon30::VirtualServer::input_point(Array<Array<int32>> &points)
+{
+	points.resize(20);
+	for (int i = 0; i < 20;i++) {
+		points[i].resize(20);
+	}
+	for (int i = 0; i < 10;i++) {
+		for (int j = 0; j < 10;j++) {
+			points[i][j] = random_number();
+		}
+	}
+}
--- MainProject/KosenProcon30/VirtualServer.hpp
+++ MainProject/KosenProcon30/VirtualServer.hpp
@@ -6,6 +6,11 @@
 namespace Procon30 {
 	class VirtualServer
 	{
-
+	private:
+		int32 player_count = 8;
+		Array<Array<int32>> points;
+		int32 random_number();
+		void write_json();
+		void input_point(Array<Array<int32>> &points);
 	};
 }
