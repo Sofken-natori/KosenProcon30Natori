28b485c3182880c729d4a2185e99f0e499241fbb
YASAI03 <ryouta.t.316@hotmail.co.jp>
2019-07-26 02:41:37 +0900

    #add 必要な変数を沢山書きました。 #add 内部処理に用いる関数を定義 #add 必要そうな関数を宣言(まだ足りてない)
    メッセージ雑で申し訳ない。
    ひとまず07/25作業分としてコミット

--- MainProject/KosenProcon30/HTTPCommunication.cpp
+++ MainProject/KosenProcon30/HTTPCommunication.cpp
@@ -1,6 +1,144 @@
 #include "HTTPCommunication.hpp"
 #include "Observer.hpp"
 #include "Game.hpp"
+Procon30::CommunicationState Procon30::HTTPCommunication::getState() const
+{
+	if (!this->future.valid())
+		return CommunicationState::Null;
+	if (this->future.wait_for(std::chrono::seconds(0)) == std::future_status::ready)
+		return CommunicationState::Done;
+	return CommunicationState::Connecting;
+}
+
+//Needs:getState() == Done
+Procon30::ConnectionStatusCode Procon30::HTTPCommunication::getResult()
+{
+	assert(this->future.valid());
+	if (this->future.valid()) {
+		CURLcode result = this->future.get();
+		uint32 code;
+		if (result != CURLE_OK) {
+			Logger << "curl_easy_perform() failed.\nUnixTime(Milli):"
+				<< Time::GetMillisecSinceEpoch();
+			return ConnectionStatusCode::ConnectionLost;
+		}
+		{
+			std::lock_guard<std::mutex> lock(receiveRawMtx);
+			size_t ofset = Min(receiveRawData.indexOf('['), receiveRawData.indexOf('{'));
+			code = Parse<uint32>(receiveRawData.substr(9, 3));
+			TextWriter writer(receiveJsonPath);
+			writer << receiveRawData.substr(ofset);
+			writer.close();
+		}
+		if (code != 200) {
+			Logger << U"Status Error:" << code;
+			if (code == 401) {
+				return ConnectionStatusCode::InvaildToken;
+			}
+			JSONReader reader(receiveJsonPath);
+			String status = reader[U"status"].get<String>();
+			uint64 startAtUnixTime = reader[U"startAtUnixTime"].get<uint64>();
+			Logger << U"status:" << status << U"\nstartAtUnixTime" << startAtUnixTime;
+			if (U"InvalidMatches" == status) {
+
+				return ConnectionStatusCode::InvailedMatches;
+			}
+			if (U"TooEarly" == status) {
+
+				return ConnectionStatusCode::TooEarly;
+			}
+			if (U"UnacceptableTime" == status) {
+
+				return ConnectionStatusCode::UnacceptableTime;
+			}
+		}
+		return ConnectionStatusCode::OK;
+	}
+	return ConnectionStatusCode::Null;
+}
+
+size_t Procon30::HTTPCommunication::callbackWrite(char* ptr, size_t size, size_t nmemb, String* stream) {
+	std::string buf;
+	size_t dataLength = size * nmemb;
+	buf.append(ptr, dataLength);
+	{
+		std::lock_guard<std::mutex> lock(receiveRawMtx);
+		stream->append(Unicode::Widen(buf));
+	}
+	return dataLength;
+}
+
+inline String Procon30::HTTPCommunication::getPostData(const FilePath& filePath) {
+	TextReader reader(filePath);
+	if (!reader) {
+		assert("send_json_file_openError");
+		exit(1);
+	}
+	String send = reader.readAll();
+	reader.close();
+	return send;
+}
+
+void Procon30::HTTPCommunication::pingServerConnectionTest()
+{
+	nowConnecting = true;
+	future = std::async(std::launch::async, [&]() {
+		return curl_easy_perform(pingHandle);
+	});
+}
+
+void Procon30::HTTPCommunication::update()
+{
+	ConnectionStatusCode CSC;
+	if (nowConnecting) {
+		switch (getState())
+		{
+		case CommunicationState::Done:
+			switch (CSC)
+			{
+			case Procon30::ConnectionStatusCode::OK:
+				switch (connectionType)
+				{
+				case Procon30::ConnectionType::Ping:
+					Logger << U"PingTest:OK";
+					break;
+				case Procon30::ConnectionType::AllMatchesInfo:
+
+					Procon30::Game::HTTPReceived();
+					break;
+				case Procon30::ConnectionType::MatchInfomation:
+					break;
+				case Procon30::ConnectionType::PostAction:
+					break;
+				case Procon30::ConnectionType::Null:
+					break;
+				}
+				break;
+			case Procon30::ConnectionStatusCode::TooEarly:
+				break;
+			case Procon30::ConnectionStatusCode::UnacceptableTime:
+				break;
+			case Procon30::ConnectionStatusCode::InvailedMatches:
+				break;
+			case Procon30::ConnectionStatusCode::InvaildToken:
+				break;
+			case Procon30::ConnectionStatusCode::ConnectionLost:
+				break;
+			case Procon30::ConnectionStatusCode::Null:
+				break;
+			}
+
+			nowConnecting = false;
+			break;
+		case CommunicationState::Null:
+
+			break;
+		case CommunicationState::Connecting:
+
+			break;
+		}
+	}
+}
 
 void Procon30::HTTPCommunication::jsonDistribute()
 {
@@ -11,7 +149,7 @@ void Procon30::HTTPCommunication::getServer()
 {
 
 
-	Procon30::Game::HTTPReceived();
+	
 }
 
 void Procon30::HTTPCommunication::postServer()
@@ -24,15 +162,35 @@ void Procon30::HTTPCommunication::postServer()
 Procon30::HTTPCommunication::HTTPCommunication()
 	:buffer(new SendBuffer())
 {
-	
+	//Setting Header
+	postList = NULL;
+	otherList = NULL;
+	otherList = curl_slist_append(otherList, (U"Authorization:" + token).narrow().c_str());
+	postList = curl_slist_append(postList, "Authorization:procon30_example_token");
+	postList = curl_slist_append(postList, "Content-Type: application/json");
+
+	//Handle-initilize
+	pingHandle = curl_easy_init();
+	curl_easy_setopt(pingHandle, CURLOPT_URL, (U"http://" + host + U"/ping").narrow().c_str());
+	curl_easy_setopt(pingHandle, CURLOPT_HTTPHEADER, otherList);
+	curl_easy_setopt(pingHandle, CURLOPT_HEADER, 1L);
+	curl_easy_setopt(pingHandle, CURLOPT_WRITEFUNCTION, callbackWrite);
+	curl_easy_setopt(pingHandle, CURLOPT_WRITEDATA, &receiveRawData);
+
+	matchesInfoHandle = curl_easy_init();
+	curl_easy_setopt(matchesInfoHandle, CURLOPT_URL, (U"http://" + host + U"/matches").narrow().c_str());
+	curl_easy_setopt(matchesInfoHandle, CURLOPT_HTTPHEADER, otherList);
+	curl_easy_setopt(matchesInfoHandle, CURLOPT_HEADER, 1L);
+	curl_easy_setopt(matchesInfoHandle, CURLOPT_WRITEFUNCTION, callbackWrite);
+	curl_easy_setopt(matchesInfoHandle, CURLOPT_WRITEDATA, &receiveRawData);
 }
 
 Procon30::HTTPCommunication::~HTTPCommunication()
 {
 }
 
-Procon30::InformationID::InformationID()
-{
-}
+//Procon30::InformationID::InformationID()
+//{
+//}
 
-inline Procon30::InformationID::InformationID(int32 teamid, Array<int32> agentids) : teamID(teamid), AgentIDs(agentids) {};
+//inline Procon30::InformationID::InformationID(int32 teamid, Array<int32> agentids) : teamID(teamid), AgentIDs(agentids) {};
--- MainProject/KosenProcon30/HTTPCommunication.hpp
+++ MainProject/KosenProcon30/HTTPCommunication.hpp
@@ -3,41 +3,117 @@
 #include "Team.hpp"
 #include "Field.hpp"
 #include "SendBuffer.hpp"
-#include "InformatonID.hpp"
 
 namespace Procon30 {
 
 	class Observer;
 	class Game;
 
+	enum class CommunicationState {
+		Null,
+		Done,
+		Connecting,
+	};
+
+	enum class ConnectionType {
+		Null,
+		Ping,
+		AllMatchesInfo,
+		MatchInfomation,
+		PostAction,
+	};
+
 	class HTTPCommunication
 	{
 	private:
 		//DONT DELETE
 		std::shared_ptr<SendBuffer> buffer;
-		//あれ
+
+		//token
 		String token;
+
+		//URL
 		String host;
-	public:
-		InformationID MyTeamInformationID;
-		Array<InformationID> EnemyTeamInformationIDs;
-		
-		std::shared_ptr<Observer> observer;
+
+		//試合の数
+		size_t matchNum;
 
 		// gameIDの変換テーブル
+		std::unordered_map<int, int> matchesConversionTable;
+
+		//ハンドル
+		CURL* pingHandle;
+		CURL* matchesInfoHandle;
+		Array<CURL*> getMatchHandles;
+		Array<CURL*> postActionHandles;
+
+		//Headerのlist
+		curl_slist* postList = NULL;
+		curl_slist* otherList = NULL;
+
+		//receiveRawDataのmutex
+		std::mutex receiveRawMtx;
+
+		//内容がWriteされる変数
+		String receiveRawData;
+
+		//受け取ったJsonのパス
+		FilePath receiveJsonPath;
+
+		//codeが入る
+		std::future<CURLcode> future;
+
+		//通信中かを判別
+		//performしたらtrue,Updateで受け取ったらfalse
+		bool nowConnecting;
+
+		//通信中の試合番号(0-indexed)
+		int32 connectionMatchNumber;
+
+		//通信を行った種別
+		ConnectionType connectionType;
+
+		//asyncのステータスを読み取ります
+		CommunicationState getState() const;
+
+		//Needs:getState() == Done
+		ConnectionStatusCode getResult();
+
+		//callback関数
+		size_t callbackWrite(char* ptr, size_t size, size_t nmemb, String* stream);
+
+		//filepathから行動情報をコピーします。
+		String getPostData(const FilePath& filePath);
+
+	public:
+		//結構頻繁に書き換わるけどnotifyガンガンやっていいの...?
+		std::shared_ptr<Observer> observer;
+
+		//変換テーブルの設定
+		//未定義
+		void setConversionTable(/* 任意の型 */);
+
+		//MatchHandleの初期化をします。
+		//Need:MatchesConversionTable.size() > 0
+		//未定義
+		void initilizeAllMatchHandles();
 
 		// ping - failedしたらプログラムを落とす。
 		void pingServerConnectionTest();
 
+		//Mainからthreadに投げるときはこれ...?
+		//随時更新
+		void update();
 
-		// host/matchesをgetしてjsonを受け取ります。
-		FilePath getAllMatchesInfomation();
+		// host/matchesをgetします。
+		//未定義
+		void getAllMatchesInfomation();
 
-		// 
+		//FormのLoop
 		void initilizeFormLoop();
-		
+
 		//receive
-		
+
 		//DONT USE:This function is not implement
 		void jsonDistribute();
 
