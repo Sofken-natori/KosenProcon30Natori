a23c4351d0ed8c4a29ccac1930783c15d038d0c1
nasatame_notepc <takanomoziretu@yahoo.co.jp>
2019-09-28 11:05:27 +0900

    #add 今後こんな感じで各自Algorithmヘッダーを使ってほしいというのをSuzukiAlgo.hppに入れました。

--- MainProject/KosenProcon30/Algorithm/SuzukiAlgorithm.cpp
+++ MainProject/KosenProcon30/Algorithm/SuzukiAlgorithm.cpp
@@ -3,3 +3,8 @@
 Procon30::AlternatelyBeamSearchAlgorithm::AlternatelyBeamSearchAlgorithm(int32 beamWidth)
 {
 }
+
+Procon30::SearchResult Procon30::AlternatelyBeamSearchAlgorithm::execute(const Game& game)
+{
+	return SearchResult();
+}
